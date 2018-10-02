c57e59b9abb1fea6d8eac5605651166dfd8b1a63
vrambo8 <s1764751@sms.ed.ac.uk>
2018-10-02 20:54:56 +0100

    added id generator for insertions

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from shortuuid import *
 # Create your models here.
+class Inserzione (models.Model):
+    titolo=models.CharField(max_length=50)
+    uuid=ShortUUID()
+    id=models.CharField(max_length=30, primary_key=True, default=uuid.uuid())
+    descrizione=models.CharField(max_length=500)
+    venditore=models.ForeignKey('Utente', on_delete=models.CASCADE, related_name="venditore")
+    utente=models.ManyToManyField('Utente', related_name="utente")
+    indirizzo=models.CharField(max_length=50)
+    prezzo=models.IntegerField()
+    class Meta:
+        unique_together=('titolo', 'id')
+    def __str__(self):
+        return self.titolo
+    
+
+class Utente (models.Model):
+    username=models.CharField(max_length=20)
+    nome=models.CharField(max_length=50)
+    cognome=models.CharField(max_length=100)
+    dataNascita=models.DateField
+    email=models.EmailField()
+    password=models.CharField(max_length=20)
+    class Meta:
+        unique_together=('username','email')
+    def __str__(self):
+        return self.username
+        
+
+    
+
+    
+    
+    
+    
--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.core.paginator import *
+from .models import *
+from django.core import paginator
 
 # Create your views here.
+def home_page(request):
+    utente=Utente.objects.get(username="BITCH")
+    context= {
+        'utente' : utente
+        }
+    return render(request, 'home.html', context)
+
+def page(request):
+   
+    ins_lista=Inserzione.objects.all()
+    paginator = Paginator(ins_lista, 50)
+    
+    page = request.GET.get('page')
+    inserzioni=paginator.get_page(page)
+    context= {
+        'inserzioni' : inserzioni
+        }
+    return render(request,'page.html', context)
+
+
+    
+    
+    
