7c8aba1eda46a4493326ae7ec7bc04963158fff6
vrambo8 <s1764751@sms.ed.ac.uk>
2018-11-22 01:35:02 +0000

    Added bidding features

--- insertion_handling/models.py
+++ insertion_handling/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django import forms
 from feed.models import Inserzione
+from django.utils.translation import ugettext_lazy as _
 from django.core.exceptions import ValidationError
 # Create your models here.
 
 class InsertionForm(forms.ModelForm):
+
     def clean(self):
         cleaned_data=super(InsertionForm, self).clean()
         titolo=cleaned_data.get('titolo')
@@ -15,13 +17,39 @@ class InsertionForm(forms.ModelForm):
         validators=[]
         
         if len(titolo)<10:
-            validators.append(ValidationError(_("Title must be at least 10 characters long"), code="insufficient title length"))
+            validators.append(ValidationError(_("Title must be at least 10 characters long!"), code="insufficient title length"))
         if max_prezzo<=0:
-            validators.append(ValidationError(_("Price must be greater than 0"), code="negative max_prezzo"))
-        if validators:
-            raise ValidationError(validators)
+            validators.append(ValidationError(_("Price must be greater than 0!"), code="negative max_prezzo"))
+        
+        raise ValidationError(validators)
         return self.cleaned_data
 
     class Meta:
         model=Inserzione
+        help_texts = {
+            'max_prezzo': '€'
+        }
         fields=['titolo', 'descrizione', 'indirizzo', 'max_prezzo', 'immagine']
+
+class BiddingForm(forms.ModelForm):
+    
+    def clean(self):
+        max_prezzo=self.cleaned_data['max_prezzo']
+            
+        if max_prezzo>=self.instance.max_prezzo:
+            raise ValidationError(_("Bid must be lower than current one! Current= " + str(self.instance.max_prezzo) + "€"), code="invalid bid")
+        if max_prezzo<=0:
+            raise ValidationError(_("Price must be greater than 0!"), code="negative new_price")
+        return self.cleaned_data
+    
+    class Meta:
+        model=Inserzione
+        help_texts = {
+            'new_price': '€'
+        }
+        fields=['max_prezzo']
+        
+                
+                
+
+
--- insertion_handling/views.py
+++ insertion_handling/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from insertion_handling.models import InsertionForm
+from .models import InsertionForm, BiddingForm
 from django.contrib.auth.decorators import login_required
 from feed.models import Inserzione
 from django.contrib import messages
+from insertion_handling.models import BiddingForm
 # Create your views here.
 def inserzione(request, ins_id):
     ins=Inserzione.objects.get(id=ins_id)
@@ -11,18 +12,27 @@ def inserzione(request, ins_id):
     user=request.user.profile
     allowed= ins.venditore==user
     if request.method=='POST':
+        bid_form=BiddingForm(data=request.POST, instance=ins)
         if 'interested' in request.POST:
             interessati.add(user)
-            redirect('inserzione', ins_id=ins_id)
-        if 'uninterested'in request.POST:
+            return redirect('inserzione', ins_id=ins_id)
+        if 'uninterested' in request.POST:
             interessati.remove(user)
-            redirect('inserzione', ins_id=ins_id)
+            return redirect('inserzione', ins_id=ins_id)
+        if 'bid' in request.POST:
+            if bid_form.is_valid():
+                bid_form.save()
+                offerenti.add(user)
+                return redirect('inserzione', ins_id=ins_id)
+    else:
+        bid_form=BiddingForm(instance=ins)       
     context= {
         'ins': ins,
         'offerenti' : offerenti.all(),
         'interessati' : interessati.all(),
         'user': user,
-        'allowed': allowed
+        'allowed': allowed,
+        'bid_form':bid_form
         
         }
     return render(request, 'inserzione.html', context)
@@ -32,8 +42,6 @@ def new_insertion(request):
     if (request.method=="POST"):
         form=InsertionForm(request.POST)
         if form.is_valid():
-            ins=form.instance
-            ins.venditore=request.user.profile
             form.save()
             
             return redirect('feed')
@@ -61,11 +69,5 @@ def update_insertion(request, ins_id):
             return redirect("inserzione", ins_id=ins_id)
     return render(request, 'update_insertion.html', {'form': form, 'ins': ins})
 
-def interested(request, ins_id):
-    user=request.user.profile
-    ins=Inserzione.objects.get(id=ins_id)
-    if (request.method=="POST"):
-        ins.interessati.append(user)
-    return redirect("inserzione", ins_id=ins_id)
-        
+
         
